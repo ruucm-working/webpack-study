7b9d48e669848c6b0b0737d07c0af9f776b7b878
ruucm <ruucm.a@gmail.com>
2021-08-11 17:57:32 +0900

    clean up

--- webpack.config.js
+++ webpack.config.js
@@ -10,49 +10,4 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/,
-        use: ["xml-loader"],
-      },
-      {
-        test: /\.toml$/,
-        type: "json",
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/,
-        type: "json",
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
-  },
 }

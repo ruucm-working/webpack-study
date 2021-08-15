dcf03382c384aea7c8f1d541b4898fe4e425b65c
ruucm <ruucm.a@gmail.com>
2021-08-15 16:49:13 +0900

    up

--- react-babel-typescript/src/index.tsx
+++ react-babel-typescript/src/index.tsx
@@ -1,6 +1,49 @@
+import path from "path"
 import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
+
+// @ts-ignore
+const req = require.context(DIRNAME, true, /\.(md|mdx|jsx)$/)
+// @ts-ignore
+const codeContext = require.context(
+  // @ts-ignore
+  "!!raw-loader!" + DIRNAME,
+  true,
+  /\.(md|mdx|jsx)$/
+)
+const routes = req
+  .keys()
+  .filter((key: any) => !/node_modules/.test(key))
+  .map((key: any) => {
+    const extname = path.extname(key)
+    const name = path.basename(key, extname)
+    const exact = name === "index"
+    const dirname = path.dirname(key).replace(/^\./, "")
+    const pathname = dirname + "/" + (exact ? "" : name)
+    let mod, Component
+    try {
+      mod = req(key)
+      Component = mod.default
+    } catch (err) {
+      console.error(err)
+    }
+    const code = codeContext(key)
+    if (typeof Component !== "function") return null
+    return {
+      key,
+      name,
+      extname,
+      dirname,
+      exact,
+      path: pathname,
+      Component,
+      code,
+    }
+  })
+  .filter(Boolean)
+
+console.log("routes", routes)
 
 const HelloWorld = () => {
   return <h1>Hello World</h1>
--- react-babel/webpack.config.js
+++ react-babel/webpack.config.js
@@ -2,10 +2,12 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: "./src/index.js",
   output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
   mode: process.env.NODE_ENV || "development",
-  resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   devServer: { contentBase: path.join(__dirname, "src") },
   module: {
     rules: [
@@ -13,6 +15,11 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
       },
       {
         test: /\.(css|scss)$/,
